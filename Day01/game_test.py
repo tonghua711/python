7704268ff59344eca879d4561d8ada17bd0d7ad7
tonghua711 <tonghua711@163.com>
2019-08-08 23:48:02 +0800

    练习 猜数字游戏

---
+++ Day01/game_test.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+
+import random
+
+secretNumber = random.randint(1,20)
+print('I am thinking of a number between 1 and 20.')
+
+for guessesTaken in range(1,7):
+    print('Take a guess.')
+    guess = int(input())
+
+    if guess < secretNumber:
+        print('你输入的数字太小')
+    elif guess > secretNumber:
+        print('你输入的数字太大')
+    else:
+        break
+if guess == secretNumber:
+    print('Good job! 你输入了：'+ str(guessesTaken) + '次才正确!')
+else:
+    print('Nope,The number I was thinking or was '+ str(secretNumber))
